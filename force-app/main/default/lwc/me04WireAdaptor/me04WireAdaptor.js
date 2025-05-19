bcf4227a113170c595bb67a6e9cb29f9e966a9d9
Satakarni Bommuluri <satakarni.byn@gmail.com>
2025-05-19 02:37:05 -0500

    04 Wire Adapter

--- force-app/main/default/lwc/me04WireAdaptor/me04WireAdaptor.js
+++ force-app/main/default/lwc/me04WireAdaptor/me04WireAdaptor.js
@@ -1,3 +1,27 @@
 import { LightningElement, wire } from 'lwc';
+import getAccounts from '@salesforce/apex/AccountController.getAccountList'; 
+const  columns = [
+        {label:'Name', fieldName:'Name', type:'text'}, 
+        {label:'Account Number', fieldName:'AccountNumber', type:'text'}, 
+        {label:'Description', fieldName:'Description', type:'text'}, 
+        {label:'City', fieldName:'BillingCity', type:'text'}, 
+        {label:'State', fieldName:'BillingState', type:'text'},
+        {label:'Zip Code', fieldName:'BillingPostalCode', type:'text'}, 
+        {label:'Revenue', fieldName:'AnnualRevenue', type:'currency'}
+        
+    ]; 
 
-export default class Me04WireAdaptor extends LightningElement {}
+export default class Me04WireAdaptor extends LightningElement {
+    recordColumns = columns; 
+   allRecords; 
+   allErrors; 
+   @wire (getAccounts) wireAccount({data, error}){
+    if(data){
+        this.allRecords = data;
+        console.log('allRecords: ' + this.allRecords);
+    }
+    if(error){
+        console.log('something went wrong' + error.message); 
+    }
+   }
+}
