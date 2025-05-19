010a84f69b0f7eef992e7410e17a26187d8975ff
Satakarni Bommuluri <satakarni.byn@gmail.com>
2025-05-19 08:37:08 -0500

    07 LDS Form

---
+++ force-app/main/default/lwc/me07LdsForm/me07LdsForm.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Me07LdsForm extends LightningElement {
+ 
+   @api recordId = '003QL00000pHfHmYAK'; 
+   @api objectApiName = 'Contact'; 
+}
